f5f71c0c6eb1ec721c2c1e3c960602e101df41c3
Adam Lawrence <adam@impostertwo.com>
2024-08-07 21:13:15 -0500

    update command

--- emccCompilationCommand.bat
+++ emccCompilationCommand.bat
@@ -1,3 +1,3 @@
-emcc -c glad.c -o glad.o -I .\Libraries\include\
-emcc -c main.cpp -o main.o -I .\Libraries\include\
-emcc main.o glad.o -o index.html -I ./Libraries/include/ -L ./Libraries/lib/ -lglfw -s USE_GLFW=3 -s FULL_ES2=1 -s MAX_WEBGL_VERSION=2 -s MIN_WEBGL_VERSION=1
+@REM emcc -c glad.c -o glad.o -I .\Libraries\include\
+@REM emcc -c main.cpp -o main.o -I .\Libraries\include\
+emcc main.cpp glad.c -o index.html -I ./Libraries/include/ -L ./Libraries/lib/ -lglfw -s USE_GLFW=3 -s FULL_ES2=1 -s MAX_WEBGL_VERSION=2 -s MIN_WEBGL_VERSION=1
